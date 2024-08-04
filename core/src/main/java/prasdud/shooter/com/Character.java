7330a67d18f1eb5e3f2a9a81d0d3dfac5d04302a
prasdud <prasdudwastaken@gmail.com>
2024-08-04 15:11:59 +0530

    added ortho camera

--- core/src/main/java/prasdud/shooter/com/Character.java
+++ core/src/main/java/prasdud/shooter/com/Character.java
@@ -1,9 +1,7 @@
 package prasdud.shooter.com;
 
-
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
-import com.badlogic.gdx.InputAdapter;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
@@ -12,10 +10,12 @@ public class Character {
     private Sprite sprite;
     private float speed = 200f;
 
-    public Character(Texture texture){
+    public Character(Texture texture) {
         sprite = new Sprite(texture);
-        sprite.setPosition(100, 100);
+        // Initial position set to center of the screen
+        sprite.setPosition(Gdx.graphics.getWidth() / 2 - sprite.getWidth() / 2, Gdx.graphics.getHeight() / 2 - sprite.getHeight() / 2);
     }
+
     public void update(float delta) {
         if (Gdx.input.isKeyPressed(Input.Keys.LEFT) || Gdx.input.isKeyPressed(Input.Keys.A)) {
             sprite.translateX(-speed * delta);
@@ -30,10 +30,19 @@ public class Character {
             sprite.translateY(-speed * delta);
         }
     }
+    public float getX() {
+        return sprite.getX() + sprite.getWidth() / 2;
+    }
+
+    public float getY() {
+        return sprite.getY() + sprite.getHeight() / 2;
+    }
+
 
     public void render(SpriteBatch batch) {
         sprite.draw(batch);
     }
+
     public void dispose() {
         sprite.getTexture().dispose();
     }
--- core/src/main/java/prasdud/shooter/com/Scene.java
+++ core/src/main/java/prasdud/shooter/com/Scene.java
@@ -1,18 +1,19 @@
 package prasdud.shooter.com;
-import com.badlogic.gdx.ApplicationAdapter;
+
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
 
-/** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class Scene implements Screen {
     private SpriteBatch batch;
     private Texture backgroundTexture;
     private Sprite backgroundSprite;
     private Character character;
-
+    private OrthographicCamera camera;
 
     @Override
     public void show() {
@@ -21,20 +22,36 @@ public class Scene implements Screen {
         backgroundSprite = new Sprite(backgroundTexture);
         Texture characterTexture = new Texture("character.png");
         character = new Character(characterTexture);
+
+        // Initialize the camera
+        camera = new OrthographicCamera(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        camera.setToOrtho(false, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
     }
 
     @Override
     public void render(float delta) {
-        ScreenUtils.clear(0.15f, 0.15f, 0.2f, 1f);
+        // Update character movement based on input
         character.update(delta);
+
+        // Move the camera to follow the character
+        camera.position.set(character.getX(), character.getY(), 0);
+        camera.update();
+
+        ScreenUtils.clear(0.15f, 0.15f, 0.2f, 1f);
+
+        batch.setProjectionMatrix(camera.combined);
+
         batch.begin();
-        backgroundSprite.draw(batch);
+        // Adjust background position according to camera position
+        batch.draw(backgroundTexture, -camera.position.x, -camera.position.y);
         character.render(batch);
         batch.end();
     }
 
     @Override
-    public void resize(int width, int height) {}
+    public void resize(int width, int height) {
+        camera.setToOrtho(false, width, height);
+    }
 
     @Override
     public void pause() {}
@@ -44,8 +61,6 @@ public class Scene implements Screen {
 
     @Override
     public void hide() {}
-
-
 
     @Override
     public void dispose() {
